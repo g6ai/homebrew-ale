516460179c73a51492d89b7c77369f920b0d0139
G. Bai <gdbaifdu@gmail.com>
2021-04-09 00:38:08 +0100

    Update wine-stable-4.rb

--- Casks/wine-stable-4.rb
+++ Casks/wine-stable-4.rb
@@ -1,6 +1,5 @@
 cask "wine-stable-4" do
   version "4.0.3"
-  sha256 "6d5ed6db6f10ff8c3460e6cc7aa33cfaf80229013bb792577b5ddb32e8141f23"
 
   url "https://dl.winehq.org/wine-builds/macosx/pool/winehq-stable-#{version}.pkg"
   name "WineHQ-stable"
